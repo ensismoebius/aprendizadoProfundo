5bdc3194b1b831f52492379461be64af172d8067
Andre Furlan <ensismoebius@gmail.com>
2023-10-04 02:33:14 -0300

    Better plots

--- simulations/test2.py
+++ simulations/test2.py
@@ -33,6 +33,10 @@ class Neuron:
 
         # The by which the membrane voltage decays each time step
         self.alpha = np.exp(-self.timeStep/self.tau)
+        
+    def set_tau(self, tau):
+        self.tau = tau
+        self.alpha = np.exp(-self.timeStep/self.tau)
 
     def fire_spike(self):
         if self.voltage > self.threshold:
@@ -58,6 +62,41 @@ class SineWaveApp(Gtk.Window):
         self.neuron = Neuron(1)
 
         vbox = Gtk.VBox()
+        
+        # Create sliders for adjusting tau and threshold
+        self.tau_scale = Gtk.Scale(
+            orientation=Gtk.Orientation.HORIZONTAL,
+            adjustment=Gtk.Adjustment(
+                value=10, 
+                lower=1, 
+                upper=100, 
+                step_increment=1, 
+                page_increment=10, 
+                page_size=0)
+            )
+        self.tau_scale.set_digits(0)
+        self.tau_scale.set_valign(Gtk.Align.START)
+        self.tau_scale.connect("value-changed", self.on_tau_changed)
+        self.tau_scale_label = Gtk.Label(label="Tau")
+        vbox.pack_start(self.tau_scale_label, expand=False, fill=False, padding=5)
+        vbox.pack_start(self.tau_scale, expand=False, fill=True, padding=5)
+
+        self.threshold_scale = Gtk.Scale(
+            orientation=Gtk.Orientation.HORIZONTAL,
+            adjustment=Gtk.Adjustment(
+                value=1, 
+                lower=0, 
+                upper=10, 
+                step_increment=0.1, 
+                page_increment=1, 
+                page_size=0)
+            )
+        self.threshold_scale.set_digits(1)
+        self.threshold_scale.set_valign(Gtk.Align.START)
+        self.threshold_scale.connect("value-changed", self.on_threshold_changed)
+        self.threshold_scale_label = Gtk.Label(label="Threshold")
+        vbox.pack_start(self.threshold_scale_label, expand=False, fill=False, padding=5)
+        vbox.pack_start(self.threshold_scale, expand=False, fill=True, padding=5)
 
         spike_label = Gtk.Label(label="Spike Amplitude:")
         vbox.pack_start(spike_label, expand=False, fill=False, padding=5)
@@ -67,13 +106,18 @@ class SineWaveApp(Gtk.Window):
         vbox.pack_start(self.spike_button, expand=False, fill=False, padding=5)
 
         # Create a Matplotlib figure and add it to the GTK window
-        # self.fig, self.ax = plt.subplots()
         self.fig, (self.ax1, self.ax2) = plt.subplots(
             2, 1, gridspec_kw={'height_ratios': [2, 1]})
+
+        # Increase the vertical spacing between subplots using subplots_adjust
+        plt.subplots_adjust(hspace=0.4)
 
         self.time = np.linspace(0, 1000, 1000)
         self.voltages = np.zeros(1000)
         self.spike_activity = np.zeros(1000)
+        
+        self.threshold_line = None  # Store the threshold line object
+        self.threshold_label = None  # Store the threshold label object
 
         # self.line, = self.ax.plot(self.time, self.voltages)
         self.line1, = self.ax1.plot(self.time, self.voltages)
@@ -98,6 +142,13 @@ class SineWaveApp(Gtk.Window):
     def on_spike_button_clicked(self, button):
         self.neuron.add_synaptic_weight(1.0)
 
+    def on_tau_changed(self, scale):
+        self.neuron.set_tau(scale.get_value())
+
+    def on_threshold_changed(self, scale):
+        self.neuron.threshold = scale.get_value()
+
+
     def update_plot(self):
         self.time[:-1] = self.time[1:]
         self.time[-1] = self.time[-1] + 1
@@ -107,6 +158,18 @@ class SineWaveApp(Gtk.Window):
 
         self.spike_activity[:-1] = self.spike_activity[1:]
         self.spike_activity[-1] = self.neuron.iterate()
+        
+        if self.threshold_line is None:
+            # Add the threshold line if it doesn't exist
+            self.threshold_line = self.ax1.axhline(y=self.neuron.threshold, color='r', linestyle='--', label='Threshold')
+            self.threshold_label = self.ax1.text(0, self.neuron.threshold, 'Threshold', color='r')
+
+        # Update the position of the threshold label based on the current x-axis limits
+        thresholdPosition, _ = self.ax1.get_xlim()
+        self.threshold_label.set_position((thresholdPosition, self.neuron.threshold + 0.01))
+        
+        # Update the threshold line position
+        self.threshold_line.set_ydata([self.neuron.threshold, self.neuron.threshold])
 
         self.line1.set_data(self.time, self.voltages)  # Update the voltage plot
         # Update the spike activity plot
