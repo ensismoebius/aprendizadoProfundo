9c4ce8de321f91095b0114ac0d07389106ce5f04
Andre Furlan <ensismoebius@gmail.com>
2023-10-03 23:19:48 -0300

    Simulation almost there!

--- simulations/test2.py
+++ simulations/test2.py
@@ -1,74 +1,79 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
+from matplotlib.backends.backend_gtk3agg import FigureCanvasGTK3Agg as FigureCanvas
 
-# Function to update the plot
-def update_plot(i):
-    spike_amplitude = spike_amplitude_adjustment.get_value()
-    plt.clf()
-    t = np.linspace(0, 2 * np.pi, 1000)
-    y = np.sin(t + 0.1 * i) + spike_amplitude * np.sin(5 * t)  # Add a temporary spike
-    plt.plot(t, y)
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    plt.title('Infinite Animated Sine Wave with Spike')
-    plt.pause(0.01)  # Adjust the pause duration to control animation speed
-    plt.draw()
+class Neuron:
+    def __init__(self):
+        self.voltage = 1
 
-# Create a GTK window
+    def add_spike(self, spike):
+        self.voltage += spike
+
+    def iterate(self):
+        self.voltage = max(self.voltage - 0.01, 0)
+
 class SineWaveApp(Gtk.Window):
     def __init__(self):
-        Gtk.Window.__init__(self, title="Sine Wave Animation")
+        Gtk.Window.__init__(self, title="Spike neuron simulation")
         self.connect("destroy", Gtk.main_quit)
 
-        # Create a VBox container
+        self.neuron = Neuron()
+
         vbox = Gtk.VBox()
 
-        # Create a label for the spike amplitude
         spike_label = Gtk.Label(label="Spike Amplitude:")
         vbox.pack_start(spike_label, expand=False, fill=False, padding=5)
 
-        # Create a adjustment for the spike amplitude
-        self.spike_amplitude_adjustment = Gtk.Adjustment(value=0.0, lower=0.0, upper=2.0, step_increment=0.1)
-        self.spike_amplitude_adjustment.connect("value-changed", self.on_spike_amplitude_changed)
+        self.spike_button = Gtk.Button(label="Add Spike")
+        self.spike_button.connect("clicked", self.on_spike_button_clicked)
+        vbox.pack_start(self.spike_button, expand=False, fill=False, padding=5)
 
-        # Create a scale widget for the spike amplitude
-        self.spike_amplitude_scale = Gtk.Scale(orientation=Gtk.Orientation.HORIZONTAL, adjustment=self.spike_amplitude_adjustment)
-        vbox.pack_start(self.spike_amplitude_scale, expand=False, fill=False, padding=5)
+
+        # Create a Matplotlib figure and add it to the GTK window
+        self.fig, self.ax = plt.subplots()
+        self.x = np.linspace(0, 1000, 1000)
+        self.y = np.zeros(1000)
+        
+        self.line, = self.ax.plot(self.x, self.y)
+        
+        self.ax.set_xlabel('Time')
+        self.ax.set_ylabel('Voltage')
+        self.ax.set_title('Spike neuron simulation')
+
+        self.canvas = FigureCanvas(self.fig)
+        vbox.pack_start(self.canvas, expand=True, fill=True, padding=5)
 
         self.add(vbox)
 
-        # Create a figure
-        self.fig = plt.figure()
+        # Initialize the animation
+        self.animation_id = GLib.idle_add(self.update_plot)
 
-        # Infinite animation loop
-        self.i = 0
-        GLib.idle_add(self.update_plot)
-
-    def on_spike_amplitude_changed(self, adjustment):
-        self.update_plot()
+    def on_spike_button_clicked(self, button):
+        self.neuron.add_spike(1.0)
 
     def update_plot(self):
-        spike_amplitude = self.spike_amplitude_adjustment.get_value()
-        plt.clf()
-        t = np.linspace(0, 2 * np.pi, 1000)
-        y = np.sin(t + 0.1 * self.i) + spike_amplitude * np.sin(5 * t)  # Add a temporary spike
-        plt.plot(t, y)
-        plt.xlabel('Time')
-        plt.ylabel('Amplitude')
-        plt.title('Infinite Animated Sine Wave with Spike')
-        plt.pause(0.01)  # Adjust the pause duration to control animation speed
-        plt.draw()
-        self.i += 1
+        self.x[:-1] = self.x[1:]
+        self.x[-1] = self.x[-1] + 1
+
+        self.y[:-1] = self.y[1:]
+        self.y[-1] = self.neuron.voltage
+
+        self.neuron.iterate()
+
+        self.line.set_data(self.x, self.y)  # Update the plot data
+
+        self.ax.relim()
+        self.ax.autoscale_view()
+
+        self.fig.canvas.draw_idle()  # Draw the updated plot
+
         return True
 
-# Create and run the GTK application
-win = SineWaveApp()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
-
+if __name__ == "__main__":
+    win = SineWaveApp()
+    win.show_all()
+    Gtk.main()
