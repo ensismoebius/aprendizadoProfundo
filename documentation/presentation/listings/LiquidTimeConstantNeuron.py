3ec198cb868e2bd3d9b83da50e5d77645d2fb476
Andre Furlan <ensismoebius@gmail.com>
2023-10-29 16:44:59 -0300

    Spiking neural networks part is ready

--- documentation/presentation/listings/LiquidTimeConstantNeuron.py
+++ documentation/presentation/listings/LiquidTimeConstantNeuron.py
@@ -40,20 +40,20 @@ import torch
 import torch.nn as nn
 
 class ESN(nn.Module):
-def __init__(self, input_size, reservoir_size, output_size):
-super(ESN, self).__init__()
-self.reservoir_size = reservoir_size
-self.W_in = nn.Linear(input_size, reservoir_size)
-self.W_res = nn.Linear(reservoir_size, reservoir_size)
-self.W_out = nn.Linear(reservoir_size, output_size)
+	def __init__(self, input_size, reservoir_size, output_size):
+		super(ESN, self).__init__()
+		self.reservoir_size = reservoir_size
+		self.W_in = nn.Linear(input_size, reservoir_size)
+		self.W_res = nn.Linear(reservoir_size, reservoir_size)
+		self.W_out = nn.Linear(reservoir_size, output_size)
 
-def forward(self, input):
-reservoir = torch.zeros((input.size(0), self.reservoir_size))
-for i in range(input.size(1)):
-input_t = input[:, i, :]
-reservoir = torch.tanh(self.W_in(input_t) + self.W_res(reservoir))
-output = self.W_out(reservoir)
-return output
+	def forward(self, input):
+		reservoir = torch.zeros((input.size(0), self.reservoir_size))
+		for i in range(input.size(1)):
+			input_t = input[:, i, :]
+			reservoir = torch.tanh(self.W_in(input_t) + self.W_res(reservoir))
+			output = self.W_out(reservoir)
+		return output
 
 # Example usage
 input_size = 10
