d8a6162124e96197b024f3b8b8c041ea625e067c
Andre Furlan <ensismoebius@gmail.com>
2023-11-05 16:09:28 -0300

    Some tests

--- simulations/article/pytorchTest3.py
+++ simulations/article/pytorchTest3.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,59 +7,59 @@ import matplotlib.pyplot as plt
 
 # Define a simple neural network architecture with dynamic size calculation
 class EEGNet(nn.Module):
-    def __init__(self, input_channels, input_timepoints):
+    def __init__(self, input_channels, input_size):
         super(EEGNet, self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=input_channels, out_channels=16, kernel_size=5)
-        self.pool1 = nn.MaxPool1d(kernel_size=2)
-        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=5)
-        self.pool2 = nn.MaxPool1d(kernel_size=2)
-
+        
+        # Calculates the features size after 1st conv. and maxpoll
+        features_size = self.get_tensor_size_after_conv(input_size,5)
+        features_size = self.get_tensor_size_after_maxpool(features_size,2)
+        # Calculates the features size after 2nd conv. and maxpoll
+        features_size = self.get_tensor_size_after_conv(features_size,5)
+        features_size = self.get_tensor_size_after_maxpool(features_size,2)
         # Calculate the size of the features after the conv and pool layers
-        self.feature_size = self._get_conv_output(input_timepoints)
-
-        self.fc1 = nn.Linear(self.feature_size, 128)
-        self.fc2 = nn.Linear(128, 26)
-
-    def _get_conv_output(self, input_timepoints):
-        # Temporarily overload the forward pass to determine the feature size
-        input = torch.autograd.Variable(torch.rand(1, 4, input_timepoints))
-        output_feat = self._forward_features(input)
-        n_size = output_feat.data.view(1, -1).size(1)
-        return n_size
-
-    def _forward_features(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        return x
+        features_size = self.get_size_after_flatten(features_size, 32)
+        
+        self.model = nn.Sequential(
+            nn.MaxPool1d(kernel_size=2),
+            nn.Conv1d(in_channels=input_channels, out_channels=16, kernel_size=5),
+            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=5),
+            nn.MaxPool1d(kernel_size=2),
+            nn.Linear(features_size, 128),
+            nn.Linear(128, 27)
+        )
 
     def forward(self, x):
-        x = self._forward_features(x)
-        x = x.view(-1, self.feature_size)  # Flatten the tensor for the fully connected layer
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        return self.model(x)
+
+    def get_tensor_size_after_conv(self, input_size, convolution_kernel_size):
+        return 1 + (input_size - convolution_kernel_size)
+
+    def get_tensor_size_after_maxpool(self, input_size, poll_kernel_size):
+        return math.floor(input_size / poll_kernel_size)
+
+    def get_size_after_flatten(self, input_size, channels_amount):
+        return input_size * channels_amount
 
 # Create the neural network
 input_channels = 4  # number of EEG channels
 input_timepoints = 250  # number of time points in each EEG sample
-net = EEGNet(input_channels=input_channels, input_timepoints=input_timepoints)
+net = EEGNet(input_channels=input_channels, input_size=input_timepoints)
 
 # Define a loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
 
 # Prepare your dataset
-X_dummy = torch.randn(100, 4, 250)  # Dummy EEG data
-y_dummy = torch.randint(0, 26, (100,))  # Dummy labels for EEG data
+# Example (dummy data and labels):
+X_dummy = torch.randn(27, 4, 250)  # 100 samples of 4-channel EEG data
+y_dummy = torch.linspace(0, 26, steps=27)  # 26 character labels
 
 # Create a DataLoader instance
 dataset = TensorDataset(X_dummy, y_dummy)
 dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 
 # Training loop
-for epoch in range(20):  # loop over the dataset multiple times
+for epoch in range(200):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(dataloader, 0):
         # get the inputs; data is a list of [inputs, labels]
