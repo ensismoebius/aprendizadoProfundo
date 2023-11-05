52d91ed4d820f1cb5e19fbd66ea81732715798e3
Andre Furlan <ensismoebius@gmail.com>
2023-11-05 17:05:08 -0300

    Convolutional neural network doing well for dummy EEG signals

--- simulations/article/pytorchTest3.py
+++ simulations/article/pytorchTest3.py
@@ -17,33 +17,39 @@ class EEGNet(nn.Module):
     def __init__(self, input_channels, input_timepoints):
         super(EEGNet, self).__init__()
         
-        # Calculate the size of the features after the 1st conv and pool layers
-        self.feature_size = get_tensor_size_after_conv(input_timepoints,5)
-        self.feature_size = get_tensor_size_after_maxpool(self.feature_size,2)
-        # Calculate the size of the features after the 2nd conv and pool layers
-        self.feature_size = get_tensor_size_after_conv(self.feature_size,5)
-        self.feature_size = get_tensor_size_after_maxpool(self.feature_size,2)
-        # Calculate the size of the features after the flatten
-        self.feature_size = self.feature_size * 32
+        ############################################################
+        ######## Calculates the size of the feature vector #########
+        ############################################################
         
-        self.conv1 = nn.Conv1d(in_channels=input_channels, out_channels=16, kernel_size=5)
-        self.pool1 = nn.MaxPool1d(kernel_size=2)
-        self.conv2 = nn.Conv1d(in_channels=16, out_channels=32, kernel_size=5)
-        self.pool2 = nn.MaxPool1d(kernel_size=2)
-        self.fl1 = nn.Flatten()
-        self.fc1 = nn.Linear(self.feature_size, 128)
-        self.relu1 = nn.ReLU()
-        self.fc2 = nn.Linear(128, 27)
+        # Calculate the size of the features after the 1st conv and pool layers
+        feature_size = get_tensor_size_after_conv(input_timepoints,5)
+        feature_size = get_tensor_size_after_maxpool(feature_size,2)
+        # Calculate the size of the features after the 2nd conv and pool layers
+        feature_size = get_tensor_size_after_conv(feature_size,5)
+        feature_size = get_tensor_size_after_maxpool(feature_size,2)
+        # Calculate the size of the features after the flatten
+        feature_size = feature_size * 32
+        
+        
+        ################################
+        ###### Creates the model #######
+        ################################
+        
+        self.model = nn.Sequential(
+            nn.Conv1d(in_channels=input_channels, out_channels=16, kernel_size=5),
+            nn.MaxPool1d(kernel_size=2),
+            
+            nn.Conv1d(in_channels=16, out_channels=32, kernel_size=5),
+            nn.MaxPool1d(kernel_size=2),
+            
+            nn.Flatten(),
+            nn.Linear(feature_size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 27)
+        )
 
     def forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = self.fl1(x)
-        x = self.fc1(x)
-        x = self.relu1(x)
-        x = self.fc2(x)
+        x = self.model(x)
         return x
 
 # Create the neural network
@@ -57,8 +63,8 @@ optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
 
 # Prepare your dataset
 # Example (dummy data and labels):
-X_dummy = torch.randn(27, 4, 250)  # 100 samples of 4-channel EEG data
-y_dummy = torch.linspace(0, 26, steps=27)  # 26 character labels
+X_dummy = torch.randn(26, 4, 250)  # 100 samples of 4-channel EEG data
+y_dummy = torch.linspace(0, 25, steps=26)  # 26 character labels
 
 # Create a DataLoader instance
 dataset = TensorDataset(X_dummy, y_dummy)
@@ -82,7 +88,7 @@ for epoch in range(200):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 10 == 9:  # print every 10 mini-batches
+        if i % 6 == 0:  # print every 6 mini-batches
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 10))
             running_loss = 0.0
